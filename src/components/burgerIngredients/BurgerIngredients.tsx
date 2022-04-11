9a732ff17559f3a6c36f97598f56efe0ac0758bb
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-04-11 18:38:31 +0300

    structure

---
+++ src/components/burgerIngredients/BurgerIngredients.tsx
@@ -0,0 +1,14 @@
+import  "./BurgerIngredients.css";
+/* import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'; */
+
+const BurgerIngredients = () => {
+    return (
+        <section /* className={BurgerIngredients} */>
+            <div
+                
+            ></div>
+        </section>
+    );
+};
+
+export default BurgerIngredients;
