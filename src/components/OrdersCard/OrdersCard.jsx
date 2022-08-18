31d92937cecf16f966881756449d752a4492af93
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-08-18 14:02:16 +0300

    OrdersCard

--- src/components/OrdersCard/OrdersCard.jsx
+++ src/components/OrdersCard/OrdersCard.jsx
@@ -2,67 +2,75 @@ import React, { useMemo } from "react";
 import Styles from "./ordersCard.module.css";
 import { useSelector } from "react-redux";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { formatDate } from "../../utils/formatDate";
 
+export default function OrdersCard({ order, status }) {
+  const allIngredients = useSelector((store) => store.ingredients.ingredients);
+  const createdAt = order.createdAt;
 
-export default function OrdersCard({order, status}) {
-  const allIngredients = useSelector(store => store.ingredients.ingredients);
-/*   console.log(order); */
-/*   const ingredients = {order.ingredients}; */
+  const orderIngredients = useMemo(() => {
+    return order?.ingredients.map((id) => {
+      return allIngredients?.find((item) => {
+        return id === item._id;
+      });
+    });
+  }, [order?.ingredients, allIngredients]);
 
-/* console.log(allIngredients); */
+  const totalOrder = useMemo(() => {
+    return orderIngredients?.reduce((sum, item) => {
+      if (item?.type === "bun") {
+        return (sum += item.price * 2);
+      }
+      return (sum += item ? item.price : 0);
+    }, 0);
+  }, [orderIngredients]);
 
-const orderIngredients = useMemo(()=> {
-  return order?.ingredients.map((id)=> {
-    console.log(id);
-    return allIngredients?.find((item)=> {
-      console.log(item.id);
-      
-      return id === item.id
-    })
-  })
-}, [order?.ingredients, allIngredients])
-
-console.log(orderIngredients);
   return (
     <>
       <section className={`${Styles.feedOrders} p-6 mr-2 mb-4`}>
         <div className={`${Styles.order} mb-6`}>
           <p className="text text_type_digits-default">#{order.number}</p>
           <p className="text text_type_main-default text_color_inactive">
-            Сегодня, 16:20 i-GMT+3
+            {formatDate(createdAt)}
           </p>
         </div>
-        <h2 className="text text_type_main-medium mb-6">
-          {order.name}
-        </h2>
+        <h2 className="text text_type_main-medium mb-6">{order.name}</h2>
+
         <div className={`${Styles.IngredientsPrice} `}>
           <ul className={`${Styles.Ingredients} `}>
-            {orderIngredients?.map((ingredient) => {
-               <li className={`${Styles.Ingredient} `} style={{ zIndex: 9 }}>
-               <img className={Styles.image} src={ingredient?.image}
-                        alt={ingredient?.name}/>
-             </li>
-            }
-
-            )}
-{/*             <li className={`${Styles.Ingredient} `} style={{ zIndex: 9 }}>
-              <img className={Styles.image} />
-            </li>
-            <li className={`${Styles.Ingredient} `} style={{ zIndex: 9 }}>
-              <img className={Styles.image} />
-            </li>
-            <li className={`${Styles.Ingredient} `} style={{ zIndex: 9 }}>
-              <img className={Styles.image} />
-            </li>
-            <li className={`${Styles.Ingredient} `} style={{ zIndex: 9 }}>
-              <img className={Styles.image} />
-            </li>
-            <li className={`${Styles.Ingredient_last} `} style={{ zIndex: 9 }}>
-              <img className={Styles.image} />
-            </li> */}
+            {orderIngredients?.map((ingredient, index) => {
+              if (index < 5) {
+                return (
+                  <li key={index} className={`${Styles.Ingredient} `}>
+                    <img
+                      className={Styles.image}
+                      src={ingredient?.image}
+                      alt={ingredient?.name}
+                    />
+                  </li>
+                );
+              }
+              if (index === 6) {
+                return (
+                  <li key={index} className={`${Styles.Ingredient_last} `}>
+                    <img
+                      className={Styles.image}
+                      src={ingredient?.image}
+                      alt={ingredient?.name}
+                    />
+                    <div className={`${Styles.IngredientOverlay}`}></div>
+                    <span
+                      className={`${Styles.IngredientCount} text text_type_main-default`}
+                    >
+                      +{orderIngredients.length - 5}
+                    </span>
+                  </li>
+                );
+              }
+            })}
           </ul>
           <div className={`${Styles.Price} ml-6`}>
-            <p className="text text_type_digits-default">480</p>
+            <p className="text text_type_digits-default mr-2">{totalOrder}</p>
             <CurrencyIcon />
           </div>
         </div>
