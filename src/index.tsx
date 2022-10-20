2cd7cd184332c1caa3304561f6ba34d63ba5d4ba
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-10-20 10:49:36 +0300

    fix: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./components/app/App";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./services/store.ts";
+import { store } from "./services/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement); //as HTMLElement или !
 root.render(
   //Оборачиваем приложение компонентом Provider из пакета react-redux
-  <Provider store={store}>
-    <BrowserRouter>
+  <Router>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </Provider>
+    </Provider>
+  </Router>
 );
 
 reportWebVitals();
