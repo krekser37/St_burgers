7b9a77ff5459c09ff511ad85573f7adc34c457b4
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-10-20 11:13:33 +0300

    fix: delete hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./components/app/App";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./services/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement); //as HTMLElement или !
+const root = ReactDOM.createRoot(
+  document.getElementById("root")!); //as HTMLElement или !
+  
 root.render(
   //Оборачиваем приложение компонентом Provider из пакета react-redux
-  <HashRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </HashRouter>
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </Router>
 );
 
 reportWebVitals();
